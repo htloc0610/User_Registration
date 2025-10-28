05de1431c84c66b8b56abd927b89d8417962252a
Huỳnh Tấn Lộc <locht1@runsystem.net>
2025-10-28 13:50:18 +0700

    Update CORS origin in main.ts to allow requests from the production frontend URL.

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   // CORS
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: 'https://registration-fe-two.vercel.app',
     credentials: process.env.CORS_CREDENTIALS === 'true' || true,
   });
 
